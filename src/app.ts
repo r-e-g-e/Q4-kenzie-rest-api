6c7d3ad290dd67ad2ebe7694b4a464c271c9f6c0
Rodrigo Tavares <rodrigotvrs70@hotmail.com>
2022-04-08 16:20:41 -0300

    fix: swagger routes

--- src/app.ts
+++ src/app.ts
@@ -10,11 +10,11 @@ const app = express();
 connectDatabase();
 
 app.use(express.json());
+app.use(routes);
 app.use(
-  "/documentation",
+  "/",
   swaggerUiExpress.serve,
   swaggerUiExpress.setup(swaggerDocument)
 );
-app.use(routes);
 
 export { app };
